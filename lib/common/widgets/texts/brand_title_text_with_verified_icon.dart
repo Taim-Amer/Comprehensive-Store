603cc66f19bf1ad8ac9348d43e853a19ea32ae18
Taim-Amer <taim.amer.000@gmail.com>
2024-09-04 11:01:28 -0700

    Brands GridView

---
+++ lib/common/widgets/texts/brand_title_text_with_verified_icon.dart
@@ -0,0 +1,48 @@
+import "package:flutter/material.dart";
+import "package:iconsax/iconsax.dart";
+import "package:t_store/common/widgets/texts/brand_title_text.dart";
+import "package:t_store/utils/constants/colors.dart";
+import "package:t_store/utils/constants/enums.dart";
+import "package:t_store/utils/constants/sizes.dart";
+
+class TBrandTitleWithVerifiedIcon extends StatelessWidget {
+  const TBrandTitleWithVerifiedIcon({
+    super.key,
+    this.maxLines = 1,
+    this.textColor,
+    this.iconColor = TColors.primary,
+    this.textAlign = TextAlign.center,
+    this.brandTextSize = TextSizes.small,
+    required this.title,
+  });
+
+  final String title;
+  final int maxLines;
+  final Color? textColor, iconColor;
+  final TextAlign? textAlign;
+  final TextSizes? brandTextSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Flexible(
+          child: TBrandTitleText(
+            title: title,
+            color: textColor,
+            maxLines: maxLines,
+            textAlign: textAlign,
+            brandTextSize: brandTextSize!,
+          ),
+        ),
+        const SizedBox(width: TSizes.xs),
+        Icon(
+          Iconsax.verify5,
+          color: iconColor,
+          size: TSizes.iconXs,
+        ),
+      ],
+    );
+  }
+}
