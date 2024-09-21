03ed4dc105fb58e55df175123786ba47ddcd2a37
Taim-Amer <taim.amer.000@gmail.com>
2024-09-21 09:06:59 -0700

    wishlist item

---
+++ lib/features/shop/screens/wishlist/wishlist.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:t_store/common/widgets/appbar/appbar.dart';
+import 'package:t_store/common/widgets/icons/circular_icon.dart';
+import 'package:t_store/features/shop/screens/home/home.dart';
+
+class FavouriteScreen extends StatelessWidget {
+  const FavouriteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: TAppBar(
+        title: Text("Wishlist", style: Theme.of(context).textTheme.headlineMedium),
+        actions: [
+          TCircularIcon(icon: Iconsax.add, onPressed: () =>  Get.to(const HomeScreen())),
+        ],
+      ),
+      body: ,
+    );
+  }
+}
--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:t_store/features/shop/screens/home/home.dart';
 import 'package:t_store/features/shop/screens/store/store.dart';
+import 'package:t_store/features/shop/screens/wishlist/wishlist.dart';
 import 'package:t_store/utils/constants/colors.dart';
 import 'package:t_store/utils/helpers/helper_functions.dart';
 
@@ -42,7 +43,7 @@ class NavigationController extends GetxController{
   final screens = [
     const HomeScreen(),
     const StoreScreen(),
-    Container(color: Colors.purple,),
+    const FavouriteScreen(),
     Container(color: Colors.yellow,),
   ];
 }
